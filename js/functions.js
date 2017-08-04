ece491bfcf8fe59c8e8ed7786a52678330ca5444
donmilorad87 <donmilorad87@gmail.com>
2017-08-04 12:08:04 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -21,7 +21,7 @@ function newSite() {
      document.getElementById('myIframe').style.opacity='1';
     document.getElementById('myIframe').style.height='650px';
     
-      document.getElementById('changeSite').style.marginTop='-40px;';
+      document.getElementById('changeSite').style.opacity='0';
 }
 
 function setMapCluster(){
@@ -36,16 +36,16 @@ for(i=0;i<obj.length;i++){
     
       (function IIFE(){
             var j = i;
-            console.log(j);
+
              var lngLat= obj[i].placeCoordinates; 
                var pieces = lngLat.split(/[\s,]+/);
             if(j < 1){
               
-               helperZ+= '{"photo_id":'+ (522081+i)+',"photo_title2":"<p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p>", "photo_title": "<h3 style=\'font-weight:700;border-bottom: 1px solid #E7E7E7; color:#f06423; text-align: leftr;\'>List of 10 closest Telenor Bank ATMs</h3><div class=\'hoverMap\'><p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#f06423;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p></div>", "photo_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "photo_file_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "longitude":' + pieces[1] +', "latitude":'+pieces[0] + ', "width": 500, "height": 350, "upload_date": "21 January 2007", "owner_id": 109117, "owner_name": "TelenorBanka", "owner_url": "http://www.telenorbanka.rs"},';
+               helperZ+= '{"photo_id":'+ (522081+i)+',"photo_title2":"<p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p>", "photo_title": "<h3 style=\'font-weight:700;border-bottom: 1px solid #E7E7E7; color:#222222; text-align: leftr;\'>List of 10 closest Telenor Bank ATMs</h3><div class=\'hoverMap\'><p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p></div>", "photo_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "photo_file_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "longitude":' + pieces[1] +', "latitude":'+pieces[0] + ', "width": 500, "height": 350, "upload_date": "21 January 2007", "owner_id": 109117, "owner_name": "TelenorBanka", "owner_url": "http://www.telenorbanka.rs"},';
      
             }
             else{
-               helperZ+= '{"photo_id":'+ (522081+i)+',"photo_title2":"<p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p>", "photo_title": "<div class=\'hoverMap\'><p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#f06423;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p></div>", "photo_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "photo_file_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "longitude":' + pieces[1] +', "latitude":'+pieces[0] + ', "width": 500, "height": 350, "upload_date": "21 January 2007", "owner_id": 109117, "owner_name": "TelenorBanka", "owner_url": "http://www.telenorbanka.rs"},';
+               helperZ+= '{"photo_id":'+ (522081+i)+',"photo_title2":"<p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p>", "photo_title": "<div class=\'hoverMap\'><p style=\'margin-bottom:6px; font-size:14px;width:98%;font-weight:700;  border-bottom:1px solid #333; color:#222222;\'><strong>placeName: </strong>' + obj[i].placeName + '</p><p style=\'font-size:12px;\'><strong>placeId: </strong>' + obj[i].placeId+'</p><p style=\'font-size:12px;\'><strong>placeAdress: </strong>' +obj[i].placeAdress + '</p><p style=\'font-size:12px;\'><strong>placeCoordinates: </strong>' + obj[i].placeCoordinates + '</p><p style=\'font-size:12px;\'><strong>placeDistanceFromUserLocation: </strong>' + obj[i].placeDistanceFromUserLocation + 'm</p></div>", "photo_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "photo_file_url": "https://www.telenor.rs/static/images/1/38/438/438_d.png", "longitude":' + pieces[1] +', "latitude":'+pieces[0] + ', "width": 500, "height": 350, "upload_date": "21 January 2007", "owner_id": 109117, "owner_name": "TelenorBanka", "owner_url": "http://www.telenorbanka.rs"},';
       
             }
         })();
@@ -66,7 +66,6 @@ helperZ = 'var data = { "count": 107,"photos": ' + helperZ.slice(0, -1) +']}';
 
              if (xhr.readyState == 4 && xhr.status == 200) {
              var krmpoticz = xhr.responseText;
-             console.log(JSON.parse(krmpoticz));
              newSite();
           
            
@@ -126,12 +125,12 @@ var zumbulkovic = document.getElementById('results').innerHTML;
  
  
  var accuracy = location.coords.accuracy;
-		console.log(accuracy);
+
 	
     ll = [location.coords.latitude, location.coords.longitude].join(',');
-    
+
     document.getElementById('demo').innerHTML ='User current cordinates: <br> Latitude: ' + location.coords.latitude + '<br> Longitude: ' + location.coords.longitude +'<br> Aaccuracy ≈ ' + accuracy +' m' ;
-	url = "https://maps.google.com/maps/api/staticmap?center=" + ll + "&maptype=roadmap&zoom=8&size=320x600&markers=" + ll + '' + res +"&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U";
+	url = "https://maps.google.com/maps/api/staticmap?center=" + ll + "&maptype=roadmap&zoom=8&size=320x600&markers=" + ll + '' + res +"&style=feature:road.highway%7Celement:geometry.fill%7Ccolor:0x96ca3a&style=feature:road.arterial%7Celement:geometry.fill%7Ccolor:0x96ca3a&&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0x96ca3a&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U";
     document.getElementById('sirakuza').setAttribute("src", url);
 }   
 function setBgMap() {
@@ -150,7 +149,7 @@ function setBgMap() {
         
     
         
-        buffer+= '<li style="font-family: "MyriadProLight";"> <b> place_name:</b> '+ item.placeName + '</br> <b>place_coordinates:</b>  ' + item.placeCoordinates + " <br> <b>Place_id: </b> " + item.placeId   + '<br> <b>Place_adress: </b>' + item.placeAdress + '<br> <b>Distance from user location: </b>' +item.placeDistanceFromUserLocation+' m <hr> <b>Static map: </b> <img src="https://maps.google.com/maps/api/staticmap?center=' + item.placeCoordinates + '&maptype=roadmap&zoom=16&size=200x200&markers=icon:http://mile.cloud-industry.com/zz_KS0_icon.ico|' + item.placeCoordinates + '&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U'  + '"></li><hr>';
+        buffer+= '<li style="font-family: "MyriadProLight";"> <b> place_name:</b> '+ item.placeName + '</br> <b>place_coordinates:</b>  ' + item.placeCoordinates + " <br> <b>Place_id: </b> " + item.placeId   + '<br> <b>Place_adress: </b>' + item.placeAdress + '<br> <b>Distance from user location: </b>' +item.placeDistanceFromUserLocation+' m <hr> <b>Static map: </b> <img src="https://maps.google.com/maps/api/staticmap?center=' + item.placeCoordinates + '&maptype=roadmap&zoom=16&size=200x200&markers=icon:http://mile.cloud-industry.com/zz_KS0_icon.ico|' + item.placeCoordinates + '&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0x96ca3a&style=feature:road.arterial%7Celement:geometry.fill%7Ccolor:0x96ca3a&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U'  + '"></li><hr>';
       } 
          
          
@@ -173,7 +172,7 @@ function sortingDsc(){
         
     
         
-        buffer+= '<li style="font-family: "MyriadProLight";"> <b> place_name:</b> '+ item.placeName + '</br> <b>place_coordinates:</b>  ' + item.placeCoordinates + " <br> <b>Place_id: </b> " + item.placeId   + '<br> <b>Place_adress: </b>' + item.placeAdress + '<br> <b>Distance from user location: </b>' +item.placeDistanceFromUserLocation+' m<hr> <b>Static map: </b> <img src="https://maps.google.com/maps/api/staticmap?center=' + item.placeCoordinates + '&maptype=roadmap&zoom=16&size=200x200&markers=icon:http://mile.cloud-industry.com/zz_KS0_icon.ico|' + item.placeCoordinates + '&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U'  + '"></li><hr>';
+        buffer+= '<li style="font-family: "MyriadProLight";"> <b> place_name:</b> '+ item.placeName + '</br> <b>place_coordinates:</b>  ' + item.placeCoordinates + " <br> <b>Place_id: </b> " + item.placeId   + '<br> <b>Place_adress: </b>' + item.placeAdress + '<br> <b>Distance from user location: </b>' +item.placeDistanceFromUserLocation+' m<hr> <b>Static map: </b> <img src="https://maps.google.com/maps/api/staticmap?center=' + item.placeCoordinates + '&maptype=roadmap&zoom=16&size=200x200&markers=icon:http://mile.cloud-industry.com/zz_KS0_icon.ico|' + item.placeCoordinates + '&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0x96ca3a&style=feature:road.arterial%7Celement:geometry.fill%7Ccolor:0x96ca3a&key=AIzaSyBTmcSBUDmoAwiaFFIbsslB4IGJRruz--U'  + '"></li><hr>';
       } 
          
          document.getElementById('resultsList').innerHTML=buffer;
